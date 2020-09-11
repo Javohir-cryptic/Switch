10678ba6abe71f52787c05dd59f90fd240a9d47e
Javohir-cryptic <javohirerkinovich@gmail.com>
2020-09-12 02:49:35 +0300

    Initial Commit

---
+++ Switch.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	char op;
+	float num1, num2;
+
+	cout << "Введите оператор (+ - * /) ";
+	cin >> op;
+
+	cout << "Введите операнды: ";
+	cin >> num1 >> num2;
+
+	switch (op)
+	{
+	case '+':
+		cout << num1 + num2;
+		break;
+	
+	 case '-':
+		cout << num1 - num2;
+		break;
+
+	case '*':
+		cout << num1 * num2;
+		break;
+
+	case '/':
+		if (num2 != 0)
+			cout << num1 / num2;
+		else
+			cout << "Division by zero";
+			
+			break;
+	
+	default:
+		// Если оператор отличается от + - * /
+		cout << "Неправильный оператор!";
+		break;
+	}
+	cout << endl;
+	return 0;
+}
